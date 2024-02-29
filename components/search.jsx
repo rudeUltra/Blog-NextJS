91658ae259580209f4fbaefbf810959540502d8b
rudeUltra <rudra21ultra@gmail.com>
2024-02-29 19:18:48 +0530

    Button UI Change

--- components/search.jsx
+++ components/search.jsx
@@ -43,7 +43,7 @@ const SearchForm = (props) => {
             <button className="ml-2 rounded-lg bg-blue-500 p-2 text-white before:ease relative h-12 w-40 overflow-hidden border border-green-500 bg-green-500 text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-green-500 hover:before:-translate-x-40" type="button" onClick={handleSearch}>
             <span className="relative z-10">Search</span>
             </button>
-            <button className="ml-2 rounded-lg bg-blue-500 p-2 text-white text-red hover:before:bg-redborder-red-500 relative h-[50px] w-40 overflow-hidden border border-red-500 bg-white px-3 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full" type="button" onClick={handleClear}>
+            <button className="ml-2 rounded-lg bg-blue-500 p-2 text-red hover:before:bg-redborder-red-500 relative h-[50px] w-40 overflow-hidden border border-red-500 bg-white px-3 text-red-500 shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-red-500 before:transition-all before:duration-500 hover:text-white hover:shadow-red-500 hover:before:left-0 hover:before:w-full" type="button" onClick={handleClear}>
             <span className="relative z-10">Clear</span>
             </button>
           </div>
